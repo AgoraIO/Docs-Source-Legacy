37ac0f07bc487aca896ae6afef2d14cb248a5c7f
Shahriar Parvez <mr.spshuvo@gmail.com>
2022-08-10 08:57:47 +0600

    :rocket: initial upload

---
+++ version-3.7.1/shared/variables/global.js
@@ -0,0 +1,123 @@
+export const COMPANY = 'Agora';
+export const URL_ROOT = 'https://docs.agora.io/en';
+export const AGORA_BACKEND = `${COMPANY} SD-RTN`;
+export const CLIENT = platform => (platform === 'unity' ? 'game' : 'app');
+
+export const API_REF_ROOT = 'https://docs.agora.io/en/video-call-4.x-beta/API%20Reference';
+export const API_REF_OLD = 'https://docs.agora.io/en/Video/API%20Reference';
+export const API_REF_WEB_ROOT = `${API_REF_OLD}/web_ng`;
+export const API_REF_ANDROID_ROOT  = `${API_REF_ROOT}/java_ng/API`;
+export const API_REF_IOS_ROOT = `${API_REF_ROOT}/ios_ng/API`;
+
+export const CONSOLE = `${COMPANY} Console`;
+export const TOKEN = 'token';
+
+export const ENGINE = `${COMPANY} Engine`;
+export const RTE = 'Real-Time Engagement';
+export const RTEC = `${RTE} Core`;
+export const RTES = `${RTE} SDK`;
+export const APP_ID_LINK = `http://agora.io`;
+export const AGORA_CONSOLE_URL = `http://console.agora.io`;
+export const API_REF_ANDROID = `http://api.agora.io/android`;
+export const LINK_APP_ID = `[Generate a temporary token](../reference/manage-agora-account#get-the-app-id)`;
+
+export const PLATFORM = 'dummy';
+
+export const VIDEO = 'Video Calling';
+export const AUDIO = 'Voice Calling';
+export const ASDK = 'Voice SDK';
+export const ILS = 'Interactive Live Streaming';
+export const ILSS = 'Interactive Live Streaming Standard';
+export const ILSP = 'Interactive Live Streaming Premium';
+export const VSDK = 'Video SDK';
+export const VSDK_RELEASE = '4.x';
+export const VSDK_PREVIOUS_RELEASE = '3.7.x';
+export const VSDK_RELEASE_FOLDER = 'video-call-4.x-beta';
+
+export const AV = 'Audio/Video';
+export const AV_URL = `${URL_ROOT}/video`;
+
+export const MESS = 'Signalling';
+export const MESS_SDK = `${MESS} SDK`;
+export const SIG = `${MESS}`;
+export const CHAT = `Chat`;
+export const PS = 'Pub-Sub';
+export const PUSH = 'Push Notifications';
+
+export const ACC = 'Acceleration';
+export const ACCM = 'Media Accelerator';
+export const ACCG = 'Global Accelerator';
+
+export const TEMPL = 'Templates';
+
+export const RTEE = 'Extensions';
+export const AA = `${COMPANY} Analytics`;
+export const WHITE = 'Interactive Whiteboard';
+export const EM = `${RTEE} Marketplace`;
+export const MS = 'Media Services';
+export const REC = 'Recording';
+export const MOD = 'Moderation';
+export const CP = 'Cloud Proxy';
+export const RTMPC = 'RTMP Converter';
+export const TRANS = 'Transcoding';
+export const OPREC = 'On-premise Recording';
+export const CREC = 'Cloud Recording';
+
+export const UCS = 'Use Cases';
+
+export const AB = 'App Builder';
+export const UIK = 'UIKit';
+export const FC = 'Flexible Classroom';
+
+export const WEB = 'Web';
+export const IOS = 'iOS';
+export const MAC = 'MacOS';
+export const AND = 'Android';
+export const WIN = 'Windows';
+
+export const ELE = 'Electron';
+export const UNI = 'Unity';
+export const FLU = 'Flutter';
+export const REA = 'React Native';
+export const COC = 'Cocos Creator';
+export const CO2_AN = 'Cocos2d-x-Android';
+export const SS = 'Server side';
+
+export const ATW = 'Technical Writer';
+export const ADR = 'DevReller';
+export const ADEV = 'Developer';
+export const AQA = 'Tester';
+export const APO = 'Product Owner';
+
+export const REPO_URL = 'https://gitlab.com/billy-the-fish/asciidoc-test';
+export const AGORA_DOCS_URL = 'https://docs.agora.io/en';
+export const DOWNLOAD_URL = 'https://download.agora.io/sdk/release';
+export const AGORA_PLATFORM_FOLDER = 'Agora%20Platform';
+
+export const API_REF_FOLDER = 'API%20Reference';
+export const API_REF_WEB = 'https://docs.agora.io/en/Video/API%20Reference/web_ng';
+
+export const APP_LINK = `${AGORA_DOCS_URL}/${AGORA_PLATFORM_FOLDER}/get_appid_token#get-the-app-id[App ID]`;
+
+export const APP_CERTIFICATE_LINK =
+  'xref:{rootdir}/reference/manage-agora-account.adoc#_get_the_app_certificate[App Certificate]';
+
+export const DEMO_BASIC_VIDEO_CALL_URL =
+  'https://webdemo.agora.io/agora-websdk-api-example-4.x/basicVideoCall/index.html';
+
+export const DEMO_PAGE_LINK = `<Link to="{{Global.DEMO_BASIC_VIDEO_CALL_URL}}"><Vg k="COMPANY" /> web demo</Link>`;
+
+export const TOKEN_GENERATE_LINK = `<Link to="https://docs.agora.io/en/Agora%20Platform/get_appid_token?platform=All#generate-a-temporary-token">Generate a temporary token</Link>`;
+export const TOKEN_GENERATE_URL = `https://docs.agora.io/en/Agora%20Platform/get_appid_token?platform=All#generate-an-rtc-temporary-token`;
+
+export const AGORA_DYNAMIC_KEY_CODE_BASE_URL =
+  'https://github.com/AgoraIO/Tools/tree/master/DynamicKey/AgoraDynamicKey';
+
+export const ANDROID_UIKIT_VERSION = '4.0.1';
+export const ANDROID_UIKIT_SDK = 'com.github.AgoraIO-Community.Android-UIKit:final';
+
+export const WEB_UIKIT = 'Agora React Web UIKit';
+
+export const IOS_MI_PACKAGE_VERSION = '4.0.0-beta.1';
+export const IOS_PACKAGE = 'NAME: AgoraRtcEngine_iOS_Beta';
+export const IOS_VOICE_PACKAGE = 'AgoraRtcEngine_iOS_Voice_Beta';
---
+++ version-3.7.1/shared/variables/product.js
@@ -0,0 +1,49 @@
+const data = {
+    'video-calling': {
+    NAME: 'Video Calling',
+    PATH: 'video-calling',
+    SDK: 'Video SDK',
+    PRODUCT: 'Video Calling',
+    BETA_API_FOLDER: 'video-call-4.x-beta',
+    IOS_PACKAGE_NAME: 'AgoraRtcEngine_iOS_Beta'
+  },
+
+  'voice-calling': {
+    NAME: 'Voice Calling',
+    PATH: 'voice-calling',
+    SDK: 'Voice SDK',
+    PRODUCT: 'Voice Calling'
+  },
+
+  'interactive-live-streaming': {
+    NAME: 'Interactive Live Streaming',
+    PATH: 'interactive-live-streaming',
+    SDK: 'Video SDK',
+    PRODUCT: 'Interactive Live Streaming',
+    IOS_PACKAGE_NAME: 'AgoraRtcEngine_iOS_Beta'
+
+  },
+
+  'analytics': {
+      NAME: 'Agora Analytics',
+      PATH: 'agora-analytics',
+      SDK: 'Agora Analytics',
+      PRODUCT: 'Agora Analytics'
+  },
+
+  'cloud-recording': {
+      NAME: 'Cloud Recording',
+      PATH: 'cloud-recording',
+      SDK: 'Cloud Recording',
+      PRODUCT: 'Cloud Recording'
+  },
+
+  'signalling': {
+      NAME: 'Signalling',
+      PATH: 'signalling',
+      SDK: 'Signalling SDK',
+      PRODUCT: 'Signalling'
+  }
+};
+
+export default data;
